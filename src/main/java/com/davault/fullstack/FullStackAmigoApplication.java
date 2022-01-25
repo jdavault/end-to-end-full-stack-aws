d176ebc2cacaa7e2c66ae4353e9ed479491178b7
Joe Davault <joe.davault@gmail.com>
2022-01-25 07:04:49 -0700

    first commit

---
+++ src/main/java/com/davault/fullstack/FullStackAmigoApplication.java
@@ -0,0 +1,13 @@
+package com.davault.fullstack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullStackAmigoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FullStackAmigoApplication.class, args);
+    }
+
+}
